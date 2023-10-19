37bc2b0bd4e4fd4ebb7fd1debf8b658464581ddb
rsp <pixgram@gmail.com>
2023-10-19 11:19:32 +0900

    add poselandmarks exist

--- main.js
+++ main.js
@@ -34,8 +34,12 @@ socket.on('connect', () => {
 })
 
 socket.on('detect', (res) => {
-  console.log('detect', res)
+  showDetectText(res)
 })
+
+function showDetectText(res) {
+  detectedText.innerText = res
+}
 
 function onResults(results) {
   // const data = tf.tensor2d([processedLandmarks]);
@@ -55,77 +59,6 @@ function onResults(results) {
   drawHolisticLandmarks(results);
 }
 
-function dtwDistance(sequence1, sequence2) {
-  // Create a distance matrix.
-  const distanceMatrix = [[]];
-
-  // Initialize the distance matrix.
-  for (let i = 0; i < sequence1.length; i++) {
-    distanceMatrix[i] = [[]];
-    for (let j = 0; j < sequence2.length; j++) {
-      distanceMatrix[i][j] = Infinity;
-    }
-  }
-
-  // Calculate the distance matrix.
-  distanceMatrix[0][0] = 0;
-  for (let i = 1; i < sequence1.length; i++) {
-    distanceMatrix[i][0] =
-      distanceMatrix[i - 1][0] + Math.abs(sequence1[i] - sequence2[0]);
-  }
-  for (let j = 1; j < sequence2.length; j++) {
-    distanceMatrix[0][j] =
-      distanceMatrix[0][j - 1] + Math.abs(sequence1[0] - sequence2[j]);
-  }
-
-  for (let i = 1; i < sequence1.length; i++) {
-    for (let j = 1; j < sequence2.length; j++) {
-      distanceMatrix[i][j] = Math.min(
-        distanceMatrix[i - 1][j] + Math.abs(sequence1[i] - sequence2[j]),
-        distanceMatrix[i][j - 1] + Math.abs(sequence1[i] - sequence2[j]),
-        distanceMatrix[i - 1][j - 1] + Math.abs(sequence1[i] - sequence2[j])
-      );
-    }
-  }
-
-  // Return the distance between the last two points in the sequences.
-  return distanceMatrix[sequence1.length - 1][sequence2.length - 1];
-}
-
-function createFeatureVectorFromLandmarks(landmarks) {
-  // Extract the positions of the landmarks.
-  const positions = landmarks.map((landmark) => landmark.position);
-
-  // Calculate the distances between the landmarks.
-  const distances = [];
-  for (let i = 0; i < positions.length; i++) {
-    for (let j = i + 1; j < positions.length; j++) {
-      distances.push(
-        Math.sqrt(
-          Math.pow(positions[i].x - positions[j].x, 2) +
-            Math.pow(positions[i].y - positions[j].y, 2)
-        )
-      );
-    }
-  }
-
-  // Calculate the angles between the landmarks.
-  const angles = [];
-  for (let i = 0; i < positions.length; i++) {
-    for (let j = i + 1; j < positions.length; j++) {
-      angles.push(
-        Math.atan2(
-          positions[j].y - positions[i].y,
-          positions[j].x - positions[i].x
-        )
-      );
-    }
-  }
-
-  // Return the feature vector.
-  return [...positions, ...distances, ...angles];
-}
-
 function drawHolisticLandmarks(results) {
   canvasCtx.save();
   canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
@@ -137,41 +70,44 @@ function drawHolisticLandmarks(results) {
     canvasElement.width,
     canvasElement.height
   );
-  // Pose...
-  drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS, {
-    color: "white",
-    lineWidth: 1,
-  });
-  drawLandmarks(
-    canvasCtx,
-    Object.values(POSE_LANDMARKS_LEFT).map(
-      (index) => results.poseLandmarks[index]
-    ),
-    {
-      visibilityMin: 0.65,
+
+  if('poseLandmarks' in results) {
+      // Pose...
+    drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS, {
       color: "white",
-      fillColor: "rgb(255,138,0)",
       lineWidth: 1,
-      radius: (data) => {
-        return lerp(data.from.z, -0.15, 0.1, 2, 1);
-      },
-    }
-  );
-  drawLandmarks(
-    canvasCtx,
-    Object.values(POSE_LANDMARKS_RIGHT).map(
-      (index) => results.poseLandmarks[index]
-    ),
-    {
-      visibilityMin: 0.65,
-      color: "white",
-      fillColor: "rgb(0,217,231)",
-      lineWidth: 1,
-      radius: (data) => {
-        return lerp(data.from.z, -0.15, 0.1, 2, 1);
-      },
-    }
-  );
+    });
+    drawLandmarks(
+      canvasCtx,
+      Object.values(POSE_LANDMARKS_LEFT).map(
+        (index) => results.poseLandmarks[index]
+      ),
+      {
+        visibilityMin: 0.65,
+        color: "white",
+        fillColor: "rgb(255,138,0)",
+        lineWidth: 1,
+        radius: (data) => {
+          return lerp(data.from.z, -0.15, 0.1, 2, 1);
+        },
+      }
+    );
+    drawLandmarks(
+      canvasCtx,
+      Object.values(POSE_LANDMARKS_RIGHT).map(
+        (index) => results.poseLandmarks[index]
+      ),
+      {
+        visibilityMin: 0.65,
+        color: "white",
+        fillColor: "rgb(0,217,231)",
+        lineWidth: 1,
+        radius: (data) => {
+          return lerp(data.from.z, -0.15, 0.1, 2, 1);
+        },
+      }
+    );
+  }
 
   // Hands...
   drawConnectors(canvasCtx, results.rightHandLandmarks, HAND_CONNECTIONS, {
